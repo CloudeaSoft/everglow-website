270e8504910decb66bb402e0632a72ca147172da
Cloudea <cloudeasoft@qq.com>
2024-09-29 16:51:45 +0800

    conf: update dependencies and project configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/icon',
+    '@nuxtjs/color-mode'
+  ],
+  ssr: true,
+  router: {
+    options: {
+      hashMode: false
+    }
+  },
+  app: {
+    baseURL: '',
+
+    head: {
+      charset: 'utf-8',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'author', content: 'everglow team' },
+        { name: 'keywords', content: 'everglow' },
+        { name: 'keywords', content: 'tmodloader-mod' },
+        { name: 'keywords', content: 'terraria' }
+      ],
+      noscript: [
+        { children: 'JavaScript is required' }
+      ]
+    },
+    keepalive: true,
+  },
+  colorMode:{
+    preference: 'light',
+    fallback: 'light',
+    hid: 'nuxt-color-mode-script',
+    globalName: '__EVERGLOW_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: 'everglow-',
+    classSuffix: '-mode',
+    storageKey: 'everglow-color-mode'
+  }
 })
