0ea4164f042a6f0a458cb35ad7c92e9666e4ed70
Cloudea <cloudeasoft@qq.com>
2024-10-14 10:56:07 +0800

    conf: update syntax config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,10 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt([
 	{
-		files: ['.vue', '.ts'],
+		files: ['**/*.vue', '**/*.ts'],
 		rules: {
 			'no-console': 'warn',
-			'object-property-newline': 'warn',
+			// 'object-property-newline': 'warn',
 			'prefer-const': 'error',
 			'semi': 'error',
 			'vue/singleline-html-element-content-newline': 'off',
