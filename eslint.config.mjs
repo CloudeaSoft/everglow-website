1b8bb530c8072dea30c047b79a28f1062bc2e94a
Cloudea <cloudeasoft@qq.com>
2024-11-25 15:01:51 +0800

    conf: update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default withNuxt([
 		files: ['**/*.vue', '**/*.ts'],
 		rules: {
 			'no-console': 'warn',
-			// 'object-property-newline': 'warn',
+			'object-property-newline': 'warn',
 			'prefer-const': 'error',
 			'semi': 'error',
 			'vue/singleline-html-element-content-newline': 'off',
