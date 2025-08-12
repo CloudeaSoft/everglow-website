41badd2f998d1014afd3dbec6732fba9aa66b55f
Cloudea <2025140062@qq.com>
2025-08-12 22:20:06 +0800

    conf: Update references

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-07-11',
+	compatibilityDate: '2024-12-20',
 	devtools: {
 		enabled: true,
 		componentInspector: false,
